d71ae3259e147effdac2d9419956cfba44125538
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-25 16:28:26 +0530

    adding data preprocessing config

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -6,3 +6,18 @@ from pathlib import Path
 class DataIngestionConfig:
     raw_data_dir : Path
     web_data_dir : Path
+
+# creatign a data preprocessingconfig class
+@dataclass(frozen= True)
+class DataProcessingConfig:
+    model_name : str
+    raw_data_dir: Path
+    processed_data_dir: Path
+    split_data_dir: Path
+
+    text_col: str
+    label_col: str
+
+    train_data_size: float
+    test_data_size: float
+    val_data_size: float
