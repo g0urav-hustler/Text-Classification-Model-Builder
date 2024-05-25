a8d501521e89696225d352e7c00107ba267c03db
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-25 16:29:17 +0530

    adding data preprocessing config

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,7 +1,8 @@
 import os
 from src.constants import *
 from src.utils.common import read_yaml, create_directories
-from src.entity.config_entity import (DataIngestionConfig)
+from src.entity.config_entity import (DataIngestionConfig,
+                                      DataProcessingConfig)
 
 
 class ConfigurationManager:
@@ -28,3 +29,29 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_processing_config(self) -> DataProcessingConfig:
+
+        config = self.config.data_processing
+        params = self.params.data_processing
+        model_name = self.params.train_model.model_name
+
+        create_directories([config.processed_data_dir, config.split_data_dir])
+
+        data_processing_config = DataProcessingConfig(
+
+            model_name = model_name,
+            raw_data_dir= config.raw_data_dir,
+            processed_data_dir= config.processed_data_dir,
+            split_data_dir= config.split_data_dir,
+
+            text_col= params.text_col,
+            label_col= params.label_col,
+
+            train_data_size= params.train_data_size,
+            test_data_size= params.test_data_size,
+            val_data_size= params.val_data_size,
+
+        )
+
+        return data_processing_config
