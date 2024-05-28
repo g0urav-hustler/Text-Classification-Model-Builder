00da4ef231386dc4a1bf18b190f781d43dbd210b
g0urav-hustler <gouravchouhan966@gmail.com>
2024-05-28 13:32:44 +0530

    update train model pipeline

--- src/pipeline/stage_03_train_model.py
+++ src/pipeline/stage_03_train_model.py
@@ -19,7 +19,7 @@ class TrainModelPipeline:
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataProcessingPipeline()
+        obj = TrainModelPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
