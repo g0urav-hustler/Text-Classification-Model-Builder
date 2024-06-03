df3f09a4216dc727c32f1dd82f36b6bfc325c032
g0urav-hustler <gouravchouhan966@gmail.com>
2024-06-03 22:48:14 +0530

    add model evaluation pipeline

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src import logger
 from src.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from src.pipeline.stage_02_data_processing import DataProcessingPipeline    
-# from src.pipeline.stage_03_train_model import TrainModelPipeline                                                
+from src.pipeline.stage_03_train_model import TrainModelPipeline 
+from src.pipeline.stage_04_model_evaluation import EvaluateModelPipeline                                               
 
 
 class InvokePipeline:
@@ -34,16 +35,27 @@ class InvokePipeline:
             raise e
         
         
-        # STAGE_NAME = "Model Training Stage"
+        STAGE_NAME = "Model Training Stage"
 
-        # try:
-        #     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        #     obj = TrainModelPipeline()
-        #     evaluation_metric = obj.main()
-        #     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-        # except Exception as e:
-        #     logger.exception(e)
-        #     raise e
+        try:
+            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+            obj = TrainModelPipeline()
+            evaluation_metric = obj.main()
+            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        except Exception as e:
+            logger.exception(e)
+            raise e
+        
+        STAGE_NAME = "Model Evaluation Stage"
+
+        try:
+            logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+            obj = EvaluateModelPipeline()
+            obj.main()
+            logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        except Exception as e:
+            logger.exception(e)
+            raise e
         
 if __name__ == '__main__':
     try:
